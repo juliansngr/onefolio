5eba282ea7b145610bd5653642b5cf54b512f13b
Julian <julian.andreduerr@web.de>
2025-06-03 12:52:20 +0200

    fixed middleware again, back to hard coded routes

--- middleware.js
+++ middleware.js
@@ -2,37 +2,24 @@ import { updateSession } from "@/lib/supabase/middleware";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  const host = request.headers.get("host") || "";
   const pathname = request.nextUrl.pathname;
 
-  const internalRoutes = ["/auth", "/dashboard", "/editor", "/statistics"];
-
-  // Interne Pfade auf onefol.io → redirect nach app.onefol.io
-  if (
-    host === "onefol.io" &&
-    internalRoutes.some((route) => pathname.startsWith(route))
-  ) {
-    const url = request.nextUrl.clone();
-    url.hostname = "app.onefol.io";
-    return NextResponse.redirect(url);
+  if (pathname === "/" || pathname.startsWith("/u")) {
+    return NextResponse.next();
   }
 
-  // Optional: öffentliche Routen auf app.onefol.io → redirect nach onefol.io
-  if (
-    host === "app.onefol.io" &&
-    !internalRoutes.some((route) => pathname.startsWith(route))
-  ) {
-    const url = request.nextUrl.clone();
-    url.hostname = "onefol.io";
-    return NextResponse.redirect(url);
-  }
-
-  // Session aktualisieren
   return await updateSession(request);
 }
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|$).*)",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
