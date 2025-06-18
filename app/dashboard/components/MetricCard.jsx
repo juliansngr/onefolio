a05bd45e8abb4dbb17afcb6f1b5d60d7377e14ae
Julian <julian.andreduerr@web.de>
2025-06-18 02:20:51 +0200

    added Plausible Analytics integration and added three metrics cards to the Dashboard

---
+++ app/dashboard/components/MetricCard.jsx
@@ -0,0 +1,35 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardAction,
+  CardFooter,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { IconTrendingUp } from "@tabler/icons-react";
+
+export default function MetricCard({ title, value, trend, description }) {
+  return (
+    <Card className="@container/card">
+      <CardHeader>
+        <CardDescription>{title}</CardDescription>
+        <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+          {value}
+        </CardTitle>
+        <CardAction>
+          <Badge variant="outline">
+            <IconTrendingUp />
+            +12.5%
+          </Badge>
+        </CardAction>
+      </CardHeader>
+      <CardFooter className="flex-col items-start gap-1.5 text-sm">
+        <div className="line-clamp-1 flex gap-2 font-medium">
+          {trend} <IconTrendingUp className="size-4" />
+        </div>
+        <div className="text-muted-foreground">{description}</div>
+      </CardFooter>
+    </Card>
+  );
+}
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,7 @@
 import Sidebar from "@/components/sidebar";
 import { createClient } from "@/lib/supabase/serverClient";
+import { getPlausibleStats } from "@/lib/getPlausibleStats";
+import MetricCard from "./components/MetricCard";
 
 export default async function Page() {
   const supabase = await createClient();
@@ -7,9 +9,52 @@ export default async function Page() {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("user_id", user.id)
+    .single();
+
+  if (!profile) {
+    return <div>No profile found</div>;
+  }
+
+  console.log(profile);
+
+  const stats = await getPlausibleStats({
+    userSlug: profile.username,
+    metrics: ["visitors"],
+    dateRange: "day",
+  });
+
+  const count = stats?.results[0]?.metrics[0] || 0;
+
   return (
     <Sidebar user={user}>
-      <h1>Dashboard</h1>
+      <div className="bg-muted min-h-svh p-6 md:p-10">
+        <div className="w-full max-w-sm md:max-w-6xl mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <MetricCard
+              title="Portfolio Views"
+              value={count}
+              trend="Trending up this week"
+              description="Portfolio views for the last 7 days"
+            />
+            <MetricCard
+              title="Visitors"
+              value={count}
+              trend="Trending up this week"
+              description="Portfolio views for the last 7 days"
+            />
+            <MetricCard
+              title="View Duration"
+              value={count}
+              trend="Trending up this week"
+              description="Portfolio views for the last 7 days"
+            />
+          </div>
+        </div>
+      </div>
     </Sidebar>
   );
 }
