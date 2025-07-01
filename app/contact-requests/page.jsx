a0a66c3b3a04d8896152d759483d5a3d017d93e7
Julian <julian.andreduerr@web.de>
2025-07-01 17:06:37 +0200

    added contact-requests dummy page

---
+++ app/contact-requests/page.jsx
@@ -0,0 +1,34 @@
+import Sidebar from "@/components/sidebar";
+import { createClient } from "@/lib/supabase/serverClient";
+
+export default async function Page() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("user_id", user.id)
+    .single();
+
+  const { data: portfolio } = await supabase
+    .from("portfolios")
+    .select("*")
+    .eq("user_id", user.id)
+    .eq("is_main", true)
+    .single();
+
+  return (
+    <Sidebar user={user}>
+      <div className="bg-muted min-h-svh p-6 md:p-10">
+        <div className="flex flex-col w-full max-w-sm md:max-w-6xl mx-auto gap-8">
+          <div className="flex flex-col gap-2 mb-6">
+            <h1 className="text-2xl font-bold">Contact Requests</h1>
+          </div>
+        </div>
+      </div>
+    </Sidebar>
+  );
+}
