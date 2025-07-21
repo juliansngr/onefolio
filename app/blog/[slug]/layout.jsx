a62afbe6ab5a05c129b178ec59e97526f1822c24
Julian <julian.andreduerr@web.de>
2025-07-21 22:50:03 +0200

    finished dynamic opengraph images

---
+++ app/blog/[slug]/layout.jsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  metadataBase: new URL("https://onefol.io"),
+};
+
+export default function BlogPostLayout({ children }) {
+  return <>{children}</>;
+}
--- app/blog/[slug]/opengraph-image.js
+++ app/blog/[slug]/opengraph-image.js
@@ -1,53 +1,67 @@
 import { ImageResponse } from "next/og";
+import { createClient } from "@/lib/supabase/serverClient";
 
 export const runtime = "edge";
 export const alt = "About Acme";
 export const contentType = "image/png";
 
-export default async function Image() {
+export default async function Image({ params }) {
+  const { slug } = await params;
+
+  const supabase = await createClient();
+  const { data: post, error } = await supabase
+    .from("blog")
+    .select("*")
+    .eq("slug", slug)
+    .single();
+
+  if (error) {
+    console.error("Error fetching blog post:", error);
+    return new Response("Error fetching blog post", { status: 500 });
+  }
+
   const fontData = await fetch(
     new URL(`../../../assets/PlusJakartaSans.ttf`, import.meta.url)
   ).then((res) => res.arrayBuffer());
 
-  const imageData = await fetch(
+  const fontBold = await fetch(
+    new URL(`../../../assets/PlusJakartaSansBold.ttf`, import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  const backgroundData = await fetch(
     new URL(`../../../assets/opengraph_empty.jpg`, import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  const logoData = await fetch(
+    new URL(`../../../assets/onefolio_icon.png`, import.meta.url)
   ).then((res) => res.arrayBuffer());
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
-        <div tw="bg-gray-50 flex w-full">
-          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-            <div tw="flex flex-col">
-              <img
-                src={imageData}
-                alt="Empty"
-                width={1200}
-                height={630}
-                style={{ objectFit: "cover" }}
-              />
-            </div>
-            <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
-              <span style={{ fontFamily: "Plus Jakarta Sans" }}>
-                Ready to dive in?
-              </span>
-              <span style={{ fontFamily: "Plus Jakarta Sans" }}>
-                Start your free trial today.
-              </span>
+      <div tw="flex w-full h-full items-center justify-center bg-white relative">
+        <img src={backgroundData} alt="Empty" width={1200} height={630} />
+        <div tw="flex flex-col absolute max-w-4xl">
+          <div tw="flex items-center">
+            <img src={logoData} alt="Logo" width={80} height={80} />
+            <h2
+              tw="text-white text-7xl ml-4"
+              style={{ fontFamily: "Plus Jakarta Sans Bold" }}
+            >
+              onefolio.
             </h2>
-            <div tw="mt-8 flex md:mt-0">
-              <div tw="flex rounded-md shadow">
-                <a tw="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white">
-                  Get started
-                </a>
-              </div>
-              <div tw="ml-3 flex rounded-md shadow">
-                <a tw="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600">
-                  Learn more
-                </a>
-              </div>
-            </div>
           </div>
+          <h2
+            tw="flex items-center text-6xl tracking-tight text-left text-white mt-24 leading-none"
+            style={{ fontFamily: "Plus Jakarta Sans Bold" }}
+          >
+            {post.title}
+          </h2>
+          <p
+            style={{ fontFamily: "Plus Jakarta Sans Medium" }}
+            tw="text-white text-4xl mt-24 flex items-center leading-none"
+          >
+            {post.excerpt}
+          </p>
         </div>
       </div>
     ),
@@ -56,10 +70,16 @@ export default async function Image() {
       height: 630,
       fonts: [
         {
-          name: "Plus Jakarta Sans",
+          name: "Plus Jakarta Sans Medium",
           data: fontData,
           style: "normal",
           weight: 400,
+        },
+        {
+          name: "Plus Jakarta Sans Bold",
+          data: fontBold,
+          style: "normal",
+          weight: 700,
         },
       ],
       emoji: "fluent",
